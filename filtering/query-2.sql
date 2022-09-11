3af20d1534d658fd4ef7221985fb9d2b6e77ef3e
brunosm16 <brunoskr23@gmail.com>
2022-09-11 20:30:18 -0300

    inner-query and not-in.

---
+++ filtering/query-2.sql
@@ -0,0 +1,10 @@
+--- Inner Query
+
+select film_id, title, replacement_cost from film
+where replacement_cost
+in (select replacement_cost from film where rental_rate > 2);
+
+
+--- Not In
+select film_id, title, description, rental_rate from film where rental_rate not in(0.99,1);
+
