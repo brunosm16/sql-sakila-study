7d1b05ce75247ac63620c9dbb07dc9185c8351d6
brunosm16 <brunoskr23@gmail.com>
2022-09-11 19:38:08 -0300

    finish query-1

--- filtering/query-1.sql
+++ filtering/query-1.sql
@@ -3,14 +3,23 @@
 */
 
 
--- select distinct c.customer_id , c.first_name, c.last_name, r.rental_date 
--- from customer c
--- 	inner join rental r 
--- 	on r.customer_id = c.customer_id
--- where date(r.rental_date) > '2005-05-25';
+select distinct c.customer_id , c.first_name, c.last_name, r.rental_date 
+from customer c
+	inner join rental r 
+	on r.customer_id = c.customer_id
+where date(r.rental_date) > '2005-05-25';
 
 select distinct c.customer_id , c.first_name, c.last_name, r.rental_date 
 from customer c
 	inner join rental r 
 	on r.customer_id = c.customer_id
 where date(r.rental_date) <> '2005-06-15';
+
+/** 
+  -- Delete with where 
+*/
+
+delete from rental
+where date(rental_date) = '2005-06-15';
+
+select * from rental where date(rental_date) = '2005-06-15';
