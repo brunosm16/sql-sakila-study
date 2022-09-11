ecf790136f3993422d1ee0bd6841572ec8c6240e
brunosm16 <brunoskr23@gmail.com>
2022-09-11 20:34:01 -0300

    built-in fn.

--- filtering/query-2.sql
+++ filtering/query-2.sql
@@ -8,3 +8,5 @@ in (select replacement_cost from film where rental_rate > 2);
 --- Not In
 select film_id, title, description, rental_rate from film where rental_rate not in(0.99,1);
 
+--- Using built-in function
+select film_id, title, description, rating from film where right(rating, 1) = 'G';
