00eb258714792709220cdbd5af075f8e3b3cc43a
brunosm16 <brunoskr23@gmail.com>
2022-09-11 20:59:08 -0300

    using is null - different case.

--- filtering/query-3.sql
+++ filtering/query-3.sql
@@ -3,3 +3,11 @@
 select rental_id, inventory_id, customer_id
 from rental
 where return_date is null;
+
+--- Using 'is null' for no returned date
+
+select rental_id, inventory_id, customer_id, return_date 
+from rental
+where return_date is null
+or return_date not between '2004-04-01' and '2005-07-01'
+order by return_date desc;
