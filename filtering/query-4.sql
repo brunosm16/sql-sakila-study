ebde12f262115a69dc188894b9388ac3c3051c89
brunosm16 <brunoskr23@gmail.com>
2022-09-11 21:12:56 -0300

    query name.

--- filtering/query-4.sql
+++ filtering/query-4.sql
@@ -3,3 +3,7 @@ from payment p
 where p.customer_id = 1 and (p.amount > 4.99 and year(p.payment_date) = 2005)
 
 select * from payment p where p.amount in(4.99, 6.99, 8.99)
+
+select first_name, last_name 
+from customer c
+where c.first_name like '_A%W%';
