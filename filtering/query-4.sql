13c4e717dc6e5255bdc57b8bff8a554372f42f49
brunosm16 <brunoskr23@gmail.com>
2022-09-11 21:07:33 -0300

    payment example 2.

--- filtering/query-4.sql
+++ filtering/query-4.sql
@@ -1,3 +1,5 @@
 select payment_id, customer_id, amount, payment_date 
 from payment p
 where p.customer_id = 1 and (p.amount > 4.99 and year(p.payment_date) = 2005)
+
+select * from payment p where p.amount in(4.99, 6.99, 8.99)
