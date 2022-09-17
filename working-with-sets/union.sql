1aab58e2c0e7b0602f347a260fd4c962b9b2b4da
brunosm16 <brunoskr23@gmail.com>
2022-09-17 17:00:52 -0300

    working with unions #1.

---
+++ working-with-sets/union.sql
@@ -0,0 +1,15 @@
+--- Actors and Customer first and last names
+select 'Actor' type, a.first_name, a.last_name
+from actor a
+union all
+select 'Customer' type, c.first_name, c.last_name
+from customer c;
+
+-- Not duplicated Actors and Customer first and last names
+select a.first_name, a.last_name
+from actor a
+where a.first_name LIKE 'J%' and a.last_name like 'D%'
+union 
+select c.first_name, c.last_name
+from customer c
+where c.first_name LIKE 'J%' and c.last_name like 'D%';
