6c41ed756eb4f3168e9545b74532fc123f48e2ec
brunosm16 <brunoskr23@gmail.com>
2022-09-17 17:34:27 -0300

    ordering union.

--- working-with-sets/union.sql
+++ working-with-sets/union.sql
@@ -13,3 +13,14 @@ union
 select c.first_name, c.last_name
 from customer c
 where c.first_name LIKE 'J%' and c.last_name like 'D%';
+
+-- Ordering union
+select a.first_name first_name, a.last_name last_name
+from actor a
+where a.first_name LIKE 'J%' and a.last_name like 'D%'
+union
+select c.first_name, c.last_name
+from customer c
+where c.first_name LIKE 'J%' and c.last_name like 'D%'
+order by first_name, last_name;
+
