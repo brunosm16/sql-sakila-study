f3fcb20e5f1e274abc879f1c91dc6e89559d8907
brunosm16 <brunoskr23@gmail.com>
2022-09-19 11:27:42 -0300

    using subqueries.

---
+++ subqueries/query-1.sql
@@ -0,0 +1,37 @@
+--- Select Highest customer_id
+select c.customer_id , concat(c.first_name, ' ', c.last_name), email 
+from customer c
+where c.customer_id = (SELECT max(customer_id) from customer);
+
+--- Highest payments by customers
+select p.amount, p.customer_id
+from payment p
+where p.amount = (SELECT max(p2.amount) from payment p2)
+group by p.amount, p.customer_id;
+
+--- Selecting only cities from Algeria
+select c.city_id, c.city from city c
+where c.city_id = (select ct.country_id from country ct where ct.country = 'Algeria');
+
+--- Cities in Brazil and Colombia
+select c.city_id, c.city, c.country_id
+from city c
+where c.country_id 
+in (
+	select ct.country_id from country ct
+	where ct.country in ('Brazil', 'Colombia')
+);
+
+--- Only customer that has payments
+select c.customer_id, c.first_name, c.last_name, p.amount
+from customer c
+	inner join payment p
+	on p.customer_id = c.customer_id
+where c.customer_id <>
+all (
+	select p2.customer_id
+	from payment p2
+	where p2.amount = 0
+)
+group by c.customer_id, p.amount
+order by p.amount asc;
