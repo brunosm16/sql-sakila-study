15480ae6d2d0b8c6520226c99877d032b8afea45
brunosm16 <brunoskr23@gmail.com>
2022-09-20 20:37:19 -0300

    right-outer-join.

--- joins-revisited/query-1.sql
+++ joins-revisited/query-1.sql
@@ -4,3 +4,10 @@ from film f
 	left outer join inventory i 
 	on f.film_id = i.film_id
 group by f.film_id;
+
+-- Right Outer Join 
+select i.inventory_id, f.title 
+from inventory i
+	right outer join film f
+	on f.film_id = i.film_id
+group by i.inventory_id, f.title;
