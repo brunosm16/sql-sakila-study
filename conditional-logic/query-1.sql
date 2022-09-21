e3a7c844c82c476d87bf400e49a4d52fea4d5817
brunosm16 <brunoskr23@gmail.com>
2022-09-20 22:04:47 -0300

    no zero denominator.

--- conditional-logic/query-1.sql
+++ conditional-logic/query-1.sql
@@ -42,3 +42,15 @@ select a.actor_id, a.first_name, a.last_name,
 	end pg_movies
 from actor a
 order by pg_movies asc;
+
+-- Average Payments with no zero denominator
+select concat(c.first_name, ' ', c.last_name) full_name, c.customer_id,
+sum(p.amount) payment_amount, count(p.amount) total_payments, 
+sum(p.amount) / 
+	case when count(p.amount) = 0 then 1 
+	else count(p.amount) end avg_payments
+from customer c
+	inner join payment p
+	on p.customer_id = c.customer_id
+group by c.customer_id
+order by avg_payments;
