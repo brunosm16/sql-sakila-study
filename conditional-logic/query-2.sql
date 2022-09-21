003281e9c244c3ded697dd43a572c33139f5355e
brunosm16 <brunoskr23@gmail.com>
2022-09-21 09:46:43 -0300

    total movies by rating.

--- conditional-logic/query-2.sql
+++ conditional-logic/query-2.sql
@@ -7,3 +7,12 @@ select name,
 	end character_set
 from language
 order by character_set;
+
+-- Movies By Rating
+select 
+	sum(case when f.rating = 'G' then 1 else 0 end) G,
+	sum(case when f.rating = 'PG' then 1 else 0 end) PG,
+	sum(case when f.rating = 'NC-17' then 1 else 0 end) 'NC-17',
+	sum(case when f.rating = 'PG-13' then 1 else 0 end) 'PG-13',
+	sum(case when f.rating = 'R' then 1 else 0 end) R
+from film f;
