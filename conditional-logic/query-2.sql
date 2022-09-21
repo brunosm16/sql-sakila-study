7c917c7318aeb7bb9515cc19a755780f4ec6f73c
brunosm16 <brunoskr23@gmail.com>
2022-09-21 09:33:30 -0300

    select character_set

---
+++ conditional-logic/query-2.sql
@@ -0,0 +1,9 @@
+-- Language by character_set
+select name, 
+	case 
+		when name in ('English', 'Italian', 'French', 'German') then 'latin1'
+		when name in ('Japanese', 'Mandarin') then 'utf8'
+		else 'Unknown'
+	end character_set
+from language
+order by character_set;
