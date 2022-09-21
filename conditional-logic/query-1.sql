56f4d8bd3cdf3b84bdac7d766a44eed8eb7e99c0
brunosm16 <brunoskr23@gmail.com>
2022-09-20 21:21:58 -0300

    rentals by active customers.

--- conditional-logic/query-1.sql
+++ conditional-logic/query-1.sql
@@ -5,3 +5,16 @@ select c.customer_id, c.first_name, c.last_name,
 		else 'notActive'
 	end customer_activity
 from customer c;
+
+-- Rental by active customers
+select c.first_name, c.last_name,
+	case 
+		when c.active = 0 then 0
+		else
+		(
+			select count(*) from rental r
+			where r.customer_id = c.customer_id
+		) 
+	end total_rentals
+from customer c
+order by total_rentals;
