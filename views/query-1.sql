2a5986981677fd1f625fae2c81e6ca44fc34d6de
brunosm16 <brunoskr23@gmail.com>
2022-09-22 13:15:56 -0300

    using customer-view to query payments.

--- views/query-1.sql
+++ views/query-1.sql
@@ -6,3 +6,10 @@ from customer;
 
 -- Check what columns are available in customer_vw
 describe customer_vw;
+
+-- Using view to count users payment amount
+select c.first_name, c.last_name, sum(p.amount) total_amount, count(*) total_payments
+from customer_vw c
+	inner join payment p
+	on p.customer_id = c.customer_id
+group by c.customer_id;
