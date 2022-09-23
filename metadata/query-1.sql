12578b43649ebbeb65832529e4eb51967566e1d6
brunosm16 <brunoskr23@gmail.com>
2022-09-23 16:10:28 -0300

    retrieve table info with schema.columns.

--- metadata/query-1.sql
+++ metadata/query-1.sql
@@ -9,3 +9,10 @@ select TABLE_NAME, IS_UPDATABLE
 from information_schema.views
 where TABLE_SCHEMA = 'sakila'
 order by table_name;
+
+-- Table information using information_schema.colums
+select TABLE_SCHEMA table_schema, COLUMN_NAME column_name, COLUMN_TYPE column_type, 
+CHARACTER_MAXIMUM_LENGTH char_max_length, NUMERIC_PRECISION num_precision,
+NUMERIC_SCALE num_scale
+from information_schema.columns
+where table_schema = 'sakila' and table_name = 'customer';
