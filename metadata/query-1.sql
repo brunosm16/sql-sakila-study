148d6c8ff3723cd5605d6efe1d8d64c7bf4caea9
brunosm16 <brunoskr23@gmail.com>
2022-09-23 16:02:14 -0300

    retrieve information about views.

--- metadata/query-1.sql
+++ metadata/query-1.sql
@@ -3,3 +3,9 @@ select table_name, table_type
 from information_schema.tables
 where TABLE_SCHEMA = 'sakila'
 order by table_name;
+
+-- Information about views
+select TABLE_NAME, IS_UPDATABLE 
+from information_schema.views
+where TABLE_SCHEMA = 'sakila'
+order by table_name;
