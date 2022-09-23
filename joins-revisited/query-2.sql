4b5f7c8b5ec7b3fce42229b9b51bb7c77bd0d0b8
brunosm16 <brunoskr23@gmail.com>
2022-09-23 10:15:32 -0300

    fix join and add not-scalar-subquery.

--- joins-revisited/query-2.sql
+++ joins-revisited/query-2.sql
@@ -3,5 +3,5 @@ select c.customer_id, c.first_name, c.last_name, sum(p.amount) payment_amount
 from customer c
 	left outer join payment p
 	on p.customer_id = c.customer_id
-group by c.first_name = p.payment_id
+group by c.first_name, c.customer_id
 order by payment_amount;
--- views/query-2.sql
+++ views/query-2.sql
@@ -10,3 +10,16 @@ select f.title, a.first_name, a.last_name, c.name from film f
 	inner join category c 
 	on c.category_id = fc.category_id
 where a.last_name = 'FAWCETT';
+
+-- Creates a view for country payments without scalar subquery
+create view country_payments_not_scalar as select c.country_id, c.country, sum(p.amount) payments
+from country c
+	inner join city ct
+	on ct.country_id  = c.country_id
+	inner join address a
+	on a.city_id = ct.city_id
+	left outer join customer cstmer
+	on cstmer.address_id = a.address_id
+	left outer join payment p
+	on p.customer_id = cstmer.customer_id
+group by c.country_id;
